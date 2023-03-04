371de856441172c35e461fdc9b74fa46578de3b6
eBoreal <jeremie@modeo.ai>
2023-03-04 14:33:29 +0100

    return several images

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ from diffusers import StableDiffusionInstructPix2PixPipeline,EulerAncestralDiscr
 
 from utils import stringToPil, pilToString
 
-# # Init is ran on server startup
-# Load  model to GPU as a global variable under pipeline
-def init():
+def init(
+):
+    """Load  model to GPU as a global variable under pipeline
+    """
     global model
     
     device = 0 if torch.cuda.is_available() else -1
@@ -27,8 +28,25 @@ def generate(
     seed: int,
     randomize_cfg: bool,
     text_cfg_scale: float,
-    image_cfg_scale: float
-    ):
+    image_cfg_scale: float,
+    num_images_per_prompt: int
+)->list:
+    """Run the model to generate the edited images. 
+
+    Args:
+        prompt (str)
+        input_image (Image.Image)
+        steps (int)
+        randomize_seed (bool)
+        seed (int)
+        randomize_cfg (bool)
+        text_cfg_scale (float)
+        image_cfg_scale (float)
+        num_images_per_prompt (int)
+
+    Returns:
+        list[dict]: list of images objects
+    """
     global model
 
     seed = random.randint(0, 100000) if randomize_seed else seed
@@ -50,28 +68,41 @@ def generate(
         'steps': steps, 
         'image_cfg_scale': image_cfg_scale, 
         'text_cfg_scale': text_cfg_scale,
-        'image_size': input_image.size})
+        'image_size': input_image.size,
+        "num_images_per_prompt": num_images_per_prompt})
 
-    edited_image = model(prompt, image=input_image, 
-            num_inference_steps=steps, 
-            image_guidance_scale=image_cfg_scale, 
-            guidance_scale=text_cfg_scale, generator=generator).images[0]
+    grid_search = [
+        (image_cfg_scale, text_cfg_scale),
+        (image_cfg_scale+.2, text_cfg_scale),
+        (image_cfg_scale-.2, text_cfg_scale),]
+    
+    edited_images = []
+    for params in grid_search:
+        res = model(prompt, image=input_image, 
+                num_inference_steps=steps, 
+                image_guidance_scale=params[0], 
+                guidance_scale=params[1], 
+                generator=generator,
+                num_images_per_prompt=num_images_per_prompt).images[0]
+        
+        edited_images.append({
+            seed:seed, 
+            text_cfg_scale:params[1], 
+            image_cfg_scale:params[0], 
+            'image': res
+        })
 
-    print("model run was successful")
+    print("model run was successfully")
 
-    return {
-        seed:seed, 
-        text_cfg_scale:text_cfg_scale, 
-        image_cfg_scale:image_cfg_scale, 
-        'image': edited_image
-        }
+    return edited_images
 
 
 # Inference is ran for every server call
 # Reference preloaded global pipeline here. 
 def inference(
     model_inputs:dict
-    ) -> dict:
+) ->list[dict]:
+    
     # Parse pipeline arguments
     # Official model inputs
     prompt = model_inputs.get('prompt', None)
@@ -81,6 +112,7 @@ def inference(
     seed=model_inputs.get('seed', None)
     randomize_cfg=model_inputs.get('randomize_cfg', False)
     randomize_seed=model_inputs.get('randomize_seed', False)
+    num_images_per_prompt=model_inputs.get('num_images', 3)
         
     # Custom
     just_test_img=model_inputs.get('test_mode', False)
@@ -113,11 +145,11 @@ def inference(
                 randomize_cfg,
                 text_cfg_scale,
                 image_cfg_scale,
+                num_images_per_prompt
         )
 
-        edited_image = results['image']
-        results['image']=pilToString(edited_image, dataUrl=toDataUrl)
-
-    # Return the results as a dictionary
-    
+        for x in results:
+            x['image']= pilToString(x['image'], dataUrl=toDataUrl)
+        
+            
     return results
